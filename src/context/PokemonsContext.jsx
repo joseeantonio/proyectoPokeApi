b8d8844da4d6d527203ff8eb1b02617a4437badd
jose <jagonzalez.alumno@iesrafaelalberti.es>
2023-03-09 16:42:43 +0100

    limpio

--- src/context/PokemonsContext.jsx
+++ src/context/PokemonsContext.jsx
@@ -14,7 +14,6 @@ const PokemonProvider = ({ children }) => {
                 let apiDetalles = await fetch(`https://pokeapi.co/api/v2/pokemon/${datos[i].name}`)
                 let datosDetalles = await apiDetalles.json()
                 datosDetalles.url=datos[i].url
-                debugger
                 todosLosPokemons.push(datosDetalles)
             }
         }finally {
